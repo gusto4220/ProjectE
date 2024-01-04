facb9e3c8bd671b9d323e74a317be9aca9b50946
gustavocruz <gustavocruz@gustavos-air.attlocal.net>
2024-01-03 20:07:50 -0600

    Initial Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,13 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Gustavo");
+		System.out.println("day1");
+		System.out.println("day2");
+
+	}
+
+}
